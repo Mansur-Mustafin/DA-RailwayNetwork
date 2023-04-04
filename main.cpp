ce9eef2e8399ce92549c0ffd0e5fd464d0cf1cd1
FranciscoS0usa <fgoncalves032@gmail.com>
2023-04-05 00:40:57 +0100

    fixed more bugs in menu

--- main.cpp
+++ main.cpp
@@ -9,16 +9,16 @@ int main() {
 
     try {
         Menu menu;
-        //menu.choose_network();
+        menu.choose_network();
     }
     catch (exception& e) {
         cout << e.what() << '\n';
     }
 
-    Graph g = Graph("test5network.csv", "test5.csv");
-    vector<string> base = {"A","D","I"}; // A -> E (D)
+    //Graph g = Graph("test5network.csv", "test5.csv");
+    //vector<string> base = {"A","D","I"}; // A -> E (D)
     //vector<int> reduce = {504};
-    cout << g.Task2_4_3(base);
+    //cout << g.Task2_4_3(base);
 
     //Graph g = Graph("Tests/network.csv", "Tests/stations.csv");
     //vector<string> base = {"Porto Campanhã" , "Vila Nova de Gaia-Devesas"}; // A -> E (D)
